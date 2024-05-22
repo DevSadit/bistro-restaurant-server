568c0c159f196f39ffcbc631baf8423ca0659cab
DevSadit <dev.sadit@gmail.com>
2024-05-23 01:34:56 +0600

    delete and read functionality added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 //
 // console.log(process.env.DB_PASS);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db(`Bistro_Boss`).collection(`menu`);
     const testimonialCollection = client.db(`Bistro_Boss`).collection(`review`);
+    const cartCollection = client.db(`Bistro_Boss`).collection(`carts`);
 
     // get the menu from database
     app.get("/menu", async (req, res) => {
@@ -37,6 +38,28 @@ async function run() {
     // get the testimonials from database
     app.get("/testimonials", async (req, res) => {
       const result = await testimonialCollection.find().toArray();
+      res.send(result);
+    });
+    // get the cart data from database
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { usrEmail: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // post the cart data to database
+    app.post("/carts", async (req, res) => {
+      const cartData = req.body;
+      const result = await cartCollection.insertOne(cartData);
+      res.send(result);
+    });
+
+    // delete cart data from server
+    app.delete(`/carts/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
